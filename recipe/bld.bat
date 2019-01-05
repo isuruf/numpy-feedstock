c300b1af6d0457631b16a68c339e169cb6124650
Isuru Fernando <isuruf@gmail.com>
2019-01-05 01:23:44 -0600

    Fix typo

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,5 +1,5 @@
-cp %LIBRARY_LIB$\libblas.lib %LIBRARY_LIB$\blas.lib
-cp %LIBRARY_LIB$\libcblas.lib %LIBRARY_LIB$\cblas.lib
+cp %LIBRARY_LIB%\libblas.lib %LIBRARY_LIB%\blas.lib
+cp %LIBRARY_LIB%\libcblas.lib %LIBRARY_LIB%\cblas.lib
 
 %PYTHON% -m pip install --no-deps --ignore-installed -v .
 if errorlevel 1 exit 1
