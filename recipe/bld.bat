66869b8a70c758d5458a62c4c38e0a02ae814f1f
Isuru Fernando <isuruf@gmail.com>
2019-01-05 01:07:06 -0600

    Workaround for missing blas.lib

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,3 +1,6 @@
+cp %LIBRARY_LIB$\libblas.lib %LIBRARY_LIB$\blas.lib
+cp %LIBRARY_LIB$\libcblas.lib %LIBRARY_LIB$\cblas.lib
+
 %PYTHON% -m pip install --no-deps --ignore-installed -v .
 if errorlevel 1 exit 1
 
